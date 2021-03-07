a7c14c3fbd5bac6da159435079a23ce346bf8ff1
Jkker <jerryjia@nyu.edu>
2021-03-08 04:22:36 +0800

    feat: index plage implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Button, Input, Tabs } from 'antd';
 import 'antd/dist/antd.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import request from 'umi-request';
 import './App.css';
@@ -16,7 +16,8 @@ function App() {
 	const handleTabClick = (key: React.SetStateAction<string>, e: any) => {
 		setActiveKey(key);
 	};
-
+	const indexSearchBarRef = useRef<any>(null);
+	const landingSearchBarRef = useRef<any>(null);
 	// Detect if user has proxy
 	const [hasProxy, setHasProxy] = useState(false);
 	useEffect(() => {
@@ -28,6 +29,8 @@ function App() {
 				userHasProxy ? frames('', userHasProxy)[0].title : frames('', userHasProxy)[1].title
 			);
 		})();
+		indexSearchBarRef?.current?.focus?.();
+		landingSearchBarRef?.current?.focus?.();
 	}, []);
 
 	// Query string functionality
@@ -36,59 +39,95 @@ function App() {
 	const handleChange = (e: any) => {
 		setKeyword(e.target.value);
 		history.push(`/?q=${e.target.value}`);
+		indexSearchBarRef?.current?.focus?.();
 	};
 
+	const handleReset = () => {
+		setKeyword('');
+		history.push(`/`);
+		indexSearchBarRef?.current?.focus?.();
+	};
 	return (
-		<div className='app-container'>
-			<div className='head-container'>
-				<Input.Search
-					placeholder='蓦然回首，那人却在，灯火阑珊处'
-					value={keyword}
-					onSearch={setKeyword}
-					onChange={handleChange}
-					size='large'
-					allowClear
-				/>
-			</div>
-			<div className='body-container'>
-				<Tabs
-					activeKey={activeKey}
-					defaultActiveKey={frames('', hasProxy)[0].title}
-					onTabClick={handleTabClick}
-					tabBarExtraContent={links(encodeURIComponent(keyword)).map(({ link, title }) => (
-						<Button key={title}>
-							<a
-								title={title}
-								key={link}
-								href={link}
-								target='_blank'
-								rel='noreferrer'
-								className='links'
-							>
-								{title}
-							</a>
-						</Button>
-					))}
+		<>
+			{keyword ? (
+				<div className='app-container'>
+					<div className='head-container'>
+						<img className='logo-left' src='favicon.png' alt='' onClick={handleReset} />
+						<Input.Search
+							placeholder='蓦然回首，那人却在，灯火阑珊处'
+							value={keyword}
+							onSearch={setKeyword}
+							onChange={handleChange}
+							size='large'
+							allowClear
+							ref={landingSearchBarRef}
+						/>
+					</div>
+					<div className='body-container'>
+						<Tabs
+							activeKey={activeKey}
+							defaultActiveKey={frames('', hasProxy)[0].title}
+							onTabClick={handleTabClick}
+							tabBarExtraContent={links(encodeURIComponent(keyword)).map(({ link, title }) => (
+								<Button key={title}>
+									<a
+										title={title}
+										key={link}
+										href={link}
+										target='_blank'
+										rel='noreferrer'
+										className='links'
+									>
+										{title}
+									</a>
+								</Button>
+							))}
+						>
+							{frames(encodeURIComponent(keyword), hasProxy)
+								.sort((a, b) => (b?.priority ?? 0) - (a?.priority ?? 0))
+								.map(({ title, link }) => (
+									<Tabs.TabPane key={title} tab={title} className='tabpane'>
+										<iframe
+											title={title}
+											className='frame'
+											src={link}
+											width='100%'
+											height='100%'
+											frameBorder='0'
+											loading='eager'
+											referrerPolicy='no-referrer'
+										/>
+									</Tabs.TabPane>
+								))}
+						</Tabs>
+					</div>
+				</div>
+			) : (
+				<div
+					className='index-page'
+					style={{
+						backgroundImage:
+							'url(https://cdnb.artstation.com/p/assets/images/images/009/312/021/large/alena-aenami-aenami-lunar.jpg?1518269866)',
+					}}
 				>
-					{frames(encodeURIComponent(keyword), hasProxy)
-						.sort((a, b) => (b?.priority ?? 0) - (a?.priority ?? 0))
-						.map(({ title, link }) => (
-							<Tabs.TabPane key={title} tab={title} className='tabpane'>
-								<iframe
-									title={title}
-									className='frame'
-									src={link}
-									width='100%'
-									height='100%'
-									frameBorder='0'
-									loading='eager'
-									referrerPolicy='no-referrer'
-								/>
-							</Tabs.TabPane>
-						))}
-				</Tabs>
-			</div>
-		</div>
+					<div className='index-head'>
+						{/* <img className='logo-center' src='favicon.png' alt='' /> */}
+						<span className='index-title'>🚀 Meta Search</span>
+					</div>
+					<div className='search-bar'>
+						<Input.Search
+							placeholder='蓦然回首，那人却在，灯火阑珊处'
+							// value={keyword}
+							onSearch={setKeyword}
+							// onChange={handleChange}
+							size='large'
+							allowClear
+							ref={indexSearchBarRef}
+						/>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
 
