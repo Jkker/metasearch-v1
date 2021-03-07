7656f2eb49b290a81717809c9fbb801c1205fddc
Jkker <jerryjia@nyu.edu>
2021-03-07 23:29:08 +0800

    implemented search

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, Input, Tabs } from 'antd';
+import 'antd/dist/antd.css';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { frames, links } from './config.js';
+
+const { TabPane } = Tabs;
+const { Search } = Input;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Get user ip & location
+	useEffect(() => {}, []);
+
+	const [query, setQuery] = useState('');
+	const handleChange = (e: any) => {
+		// console.log(e.target.value);
+		setQuery(e.target.value);
+		console.log(query);
+	};
+	return (
+		<div className='app-container'>
+			<div className='head-container'>
+				<Search
+					placeholder='那人却在，灯火阑珊处'
+					onSearch={setQuery}
+					onChange={handleChange}
+					size='large'
+					allowClear
+				/>
+			</div>
+			<div className='body-container'>
+				<Tabs
+					tabBarExtraContent={links(encodeURIComponent(query)).map(({ link, title }) => (
+						<Button key={title}>
+							<a
+								title={title}
+								key={link}
+								href={link}
+								target='_blank'
+								rel='noreferrer'
+								className='links'
+							>
+								{title}
+							</a>
+						</Button>
+					))}
+				>
+					{frames(encodeURIComponent(query)).map(({ title, link }) => (
+						<TabPane key={link} tab={title} className='tabpane'>
+							<iframe
+								title={title}
+								className='frame'
+								src={link}
+								width='100%'
+								height='100%'
+								frameBorder='0'
+								loading='eager'
+								referrerPolicy='no-referrer'
+							/>
+						</TabPane>
+					))}
+				</Tabs>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/searchProvider.tsx
@@ -0,0 +1,5 @@
+
+export default function searchProvider(){
+  // const searchContext = createContext()
+
+}
